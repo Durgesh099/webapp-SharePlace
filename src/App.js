f9f27c4136e0246736ddf2e0f4dc6b841e28e794
Durgesh099 <99504330+Durgesh099@users.noreply.github.com>
2023-08-02 21:43:31 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -9,20 +9,26 @@ import {
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
 import Navigation from './shared/Navigation';
+import UserPlaces from './places/pages/UserPlaces';
 
 const App = () => {
   return (
     <Router>
       <Navigation/>
+      <main>
       <Switch>
         <Route path="/" exact>
           <Users />
+        </Route>
+        <Route path="/:userId/places" exact>
+          <UserPlaces />
         </Route>
         <Route path="/places/new" exact>
           <NewPlace />
         </Route>
         <Redirect to="/" />
       </Switch>
+      </main>
     </Router>
   );
 };
