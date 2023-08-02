02a8bcc9030ee2dcfe839cf6ef696a43f32e6ba5
Durgesh099 <99504330+Durgesh099@users.noreply.github.com>
2023-08-02 14:28:01 +0530

    Add files via upload

---
+++ src/shared/Sidedraw.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import './Sidedraw.css';
+
+const Sidedraw =(props)=>{
+    const content = <aside className='sd'>{props.children}</aside>;
+
+    return ReactDOM.createPortal(content, document.getElementById('drawer'));
+};
+
+export default Sidedraw;
