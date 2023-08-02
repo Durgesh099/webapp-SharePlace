c9214da3775d2098cb5a748b67448590e5d02193
Durgesh099 <99504330+Durgesh099@users.noreply.github.com>
2023-08-03 00:52:20 +0530

    Add files via upload

---
+++ src/places/PlacesList.js
@@ -0,0 +1,36 @@
+import React from "react";
+import PlacesItems from "./PlacesItems";
+import Card from "../shared/components/Card";
+import './PlacesList.css';
+
+const PlacesList = props =>{
+    if(props.items.length===0){
+        return (
+        <div>
+            <Card>
+                <h2>No places found. Maybe create one?</h2>
+                <button>Share Place</button>
+            </Card>
+        </div>
+        );
+    }
+
+    return (
+        <ul className="place-list">
+      {props.items.map(place => (
+        <PlacesItems
+          key={place.id}
+          id={place.id}
+          image={place.imageUrl}
+          title={place.title}
+          description={place.description}
+          address={place.address}
+          creatorId={place.creator}
+          coordinates={place.location}
+        />
+      ))}
+    </ul>
+    );
+};
+
+export default PlacesList;
