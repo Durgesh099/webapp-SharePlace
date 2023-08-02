db1fa81e854341545a8e4a4817b38800078cf2a2
Durgesh099 <99504330+Durgesh099@users.noreply.github.com>
2023-08-02 21:44:46 +0530

    Add files via upload

---
+++ src/places/pages/UserPlaces.js
@@ -0,0 +1,35 @@
+import React from "react";
+import PlacesList from "../components/PlacesList";
+
+const PLACES = [
+    {
+        id:'p1',
+        title: 'Osho Garden',
+        description: 'Wonderful nature spot',
+        imageUrl: 'https://lh3.googleusercontent.com/p/AF1QipNtm6B4FA2H1ZS94AuHsvtCfvwkonU39qYQT-Mh=s1360-w1360-h1020',
+        address: 'D. H. Dhunjibhoy Road, Between Lane Number 2 and 3, Koregaon Rd, Koregaon Park, Pune, Maharashtra 411001',
+        location: {
+            lat:18.5358733,
+            lng:73.8546432
+        },
+        creator: 'u1'
+    },
+    {
+        id:'p2',
+        title: 'SGS Mall',
+        description: 'Shopping complex',
+        imageUrl: 'https://lh5.googleusercontent.com/p/AF1QipM9ndLlUV30d9BzJfMnRag_J_aL6xOzGoOKMDvR=w408-h544-k-no',
+        address: '231, Moledina Rd, Camp, Pune, Maharashtra 411001',
+        location: {
+            lat:18.5195803,
+            lng:73.8773738
+        },
+        creator: 'u2'
+    }
+];
+
+const UserPlaces = () =>{
+    return <PlacesList items={PLACES} />;
+};
+
+export default UserPlaces;
