c48195de8d2883d19d4a0e5669249c2b2c3ac3e8
Durgesh099 <99504330+Durgesh099@users.noreply.github.com>
2023-08-11 01:01:12 +0530

    Add files via upload

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,19 +1,66 @@
 import React from 'react';
-
+import { VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../../shared/FormElements/Validators';
 import Input from '../../shared/FormElements/Input';
+import Button from '../../shared/components/Button';
+import {useForm} from '../../shared/hooks/form-hook';
+import './NewPlace.css';
 
-import './PlaceForm.css';
 
 const NewPlace = () => {
+  const[formState,inputHandler]=useForm(
+    {
+      title: {
+        value: '',
+        isValid: false
+      },
+      description: {
+        value: '',
+        isValid: false
+      },
+      address: {
+        value:'',
+        isValid:false
+      }
+    },
+    false
+  );
+
+const submitHandler = event=>{
+    event.preventDefault();
+    console.log(formState.inputs);
+}
+
   return (
-    <form className="place-form">
+    <form className="place-form" onSubmit={submitHandler}>
+      <h2>New Place</h2>
       <Input
         id="title"
         element="input"
         type="text"
         label="Title"
-        errorText="Please enter valid text"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid title."
+        onInput={inputHandler}
       />
+      <Input
+        id="description"
+        element="textarea"
+        label="Description"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid description (at least 5 characters)."
+        onInput={inputHandler}
+      />
+      <Input
+        id="address"
+        element="textarea"
+        label="Address"
+        validators={[VALIDATOR_MINLENGTH(2)]}
+        errorText="Please enter a valid address (at least 2 characters)."
+        onInput={inputHandler}
+      />
+      <Button type="submit" disabled={!formState.isValid}>
+        ADD PLACE
+      </Button>
     </form>
   );
 };
--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -18,10 +18,7 @@ const PLACES = [
         description: 'Shopping complex',
         imageUrl: 'https://lh5.googleusercontent.com/p/AF1QipM9ndLlUV30d9BzJfMnRag_J_aL6xOzGoOKMDvR=w408-h544-k-no',
         address: '231, Moledina Rd, Camp, Pune, Maharashtra 411001',
-        location: {
-            lat:18.5195803,
-            lng:73.8773738
-        },
+        location: 'https://www.google.com/maps/place/SGS+Mall,+231,+Moledina+Rd,+Camp,+Pune,+Maharashtra+411001/@18.5195803,73.8747989,17z/data=!3m1!4b1!4m6!3m5!1s0x3bc2c04ff9abd0cd:0xe76fee73118c11d3!8m2!3d18.5195803!4d73.8773738!16s%2Fg%2F1ptxqbhsl?entry=ttu',
         creator: 'u2'
     }
 ];
