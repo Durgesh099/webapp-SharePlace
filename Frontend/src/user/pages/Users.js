c2581d2b813812e2a8b433ceabf3673a5db02764
Durgesh <durgeshsonarjee@gmail.com>
2023-10-06 15:23:56 +0530

    Added loadingSpinner, get post fetch apis, and error modal

--- Frontend/src/user/pages/Users.js
+++ Frontend/src/user/pages/Users.js
@@ -1,18 +1,52 @@
-import React from 'react';
-
+import React, { useEffect , useState} from 'react';
+import ErrorModal from '../../shared/UIElements/ErrorModal';
+import LoadingSpinner from '../../shared/UIElements/LoadingSpinner';
 import UsersList from '../components/UsersList';
+import '../../shared/UIElements/LoadingSpinner.css'
 
 const Users = () => {
-  const USERS = [
-    {
-      id: 'u1',
-      name: 'Gojo',
-      places: 3,
-      image: 'https://sm.mashable.com/t/mashable_in/photo/default/8ede538fcf75a0a1bd812810edb50cb7_733r.720.jpg'
-    }
-  ];
+  const [Loading, setLoading] =  useState(false)
+  const [error, setError] = useState()
+  const [loadedUsers, setLoadedUsers] = useState()
 
-  return <UsersList items={USERS} />;
+  useEffect(()=>{
+    const sendRequest = async () =>{
+      setLoading(true)
+      try{
+        const response = await fetch('http://localhost:3000/api/users')
+
+        const responseData = await response.json()
+
+        if(!response.ok){
+          throw new Error(responseData.message)
+        }
+
+        setLoadedUsers(responseData.users)
+
+      }catch(err){
+        setError(err.message)
+      }
+      setLoading(false)
+    }
+    sendRequest()
+  },[])
+
+  const errorHandler = () =>{
+    setError(null)
+  }
+
+  return (
+    <React.Fragment>
+      <ErrorModal error={error} onClear={errorHandler}/>
+      {Loading && (
+        <div className='center'>
+          <LoadingSpinner asOverlay/>
+        </div>
+      )}
+
+      {!Loading && loadedUsers && <UsersList items={loadedUsers}/>}
+    </React.Fragment>  
+  );
 };
 
 export default Users;
