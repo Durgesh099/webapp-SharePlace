d1d753a86d0e7ac0624aeddf0c37ef84195131cc
Durgesh <durgeshsonarjee@gmail.com>
2024-01-11 15:53:18 +0530

    Final backend

---
+++ Frontend/build/precache-manifest.4e091c0053430e68c07ec33a01a7c045.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18a9cc18e2e070ed3d90b9ed20518360",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b252699e226d18ac6f9f",
+    "url": "/static/css/3.ecfc0b6d.chunk.css"
+  },
+  {
+    "revision": "e089f56750118288cda5",
+    "url": "/static/css/4.a3dbf723.chunk.css"
+  },
+  {
+    "revision": "15fd0a86386f199b70fa",
+    "url": "/static/css/5.408e3a6c.chunk.css"
+  },
+  {
+    "revision": "0c877ba7ad900e0f2d4d",
+    "url": "/static/css/6.c5cee352.chunk.css"
+  },
+  {
+    "revision": "71ea4b949b936405d198",
+    "url": "/static/css/main.f76c2f9e.chunk.css"
+  },
+  {
+    "revision": "9b83a446e8843125750a",
+    "url": "/static/js/2.8963224a.chunk.js"
+  },
+  {
+    "revision": "b252699e226d18ac6f9f",
+    "url": "/static/js/3.7ab0c410.chunk.js"
+  },
+  {
+    "revision": "e089f56750118288cda5",
+    "url": "/static/js/4.0931f302.chunk.js"
+  },
+  {
+    "revision": "15fd0a86386f199b70fa",
+    "url": "/static/js/5.0dbbadf7.chunk.js"
+  },
+  {
+    "revision": "0c877ba7ad900e0f2d4d",
+    "url": "/static/js/6.f0b91cb8.chunk.js"
+  },
+  {
+    "revision": "86bc46171c2dff22c534",
+    "url": "/static/js/7.8c37d98c.chunk.js"
+  },
+  {
+    "revision": "71ea4b949b936405d198",
+    "url": "/static/js/main.50a62e44.chunk.js"
+  },
+  {
+    "revision": "9b538e6a9a4e54e08822",
+    "url": "/static/js/runtime-main.cd28e7f6.js"
+  }
+]);
